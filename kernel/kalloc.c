58b526ec7ac7a9553cc6d2450b0c5ccc4bb64ec9
delelelabombom <1357173227@qq.com>
2022-04-07 19:41:00 +0800

    finished cow lab

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -23,6 +23,28 @@ struct {
   struct run *freelist;
 } kmem;
 
+int refcount[PHYSTOP / PGSIZE];
+
+void incref(uint64 pa){
+  int pn = pa / PGSIZE;
+  acquire(&kmem.lock);
+  if (pa >= PHYSTOP || refcount[pn] < 1)
+    panic("incref\n");
+  refcount[pn]++;
+  release(&kmem.lock);
+}
+
+int getrefcount(uint64 pa){
+  int pn = pa / PGSIZE;
+  int rc;
+  acquire(&kmem.lock);
+  if (pa >= PHYSTOP)
+    panic("getrefcount\n");
+  rc = refcount[pn];
+  release(&kmem.lock);
+  return rc;
+}
+
 void
 kinit()
 {
@@ -35,8 +57,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE){
+    refcount[(uint64)p / PGSIZE] = 1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -51,6 +75,17 @@ kfree(void *pa)
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
+  acquire(&kmem.lock);
+  int pn = (uint64)pa / PGSIZE;
+  if (refcount[pn] < 1)
+    panic("kfree: ref = 0\n");
+  refcount[pn] -= 1;
+  int tmp = refcount[pn];
+  release(&kmem.lock);
+
+  if (tmp > 0)
+    return;
+  
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
@@ -72,8 +107,15 @@ kalloc(void)
 
   acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r){
     kmem.freelist = r->next;
+    int index = (uint64)r / PGSIZE;
+    if (refcount[index] != 0)
+    {
+      panic("kalloc: refcount not 0 \n");
+    }
+    refcount[index] = 1;
+  }
   release(&kmem.lock);
 
   if(r)
--- kernel/trap.c
+++ kernel/trap.c
@@ -29,6 +29,38 @@ trapinithart(void)
   w_stvec((uint64)kernelvec);
 }
 
+int cowfault(pagetable_t pagetable, uint64 va)
+{
+  if (va >= MAXVA)
+  {
+    return -1;
+  }
+  pte_t *pte = walk(pagetable, va, 0);
+  if (pte == 0)
+    return -1;
+  if ((*pte & PTE_U) == 0 || (*pte & PTE_V) == 0 || (*pte & PTE_C) == 0)
+  {
+    return -1;
+  }
+  *pte = *pte & ~PTE_C;
+  uint64 pa1 = PTE2PA(*pte);
+  uint64 pa2 = (uint64)kalloc();
+  //incref(pa2);
+  if (pa2 == 0)
+  {
+    printf("cow kalloc failed\n");
+    return -1;
+  }
+  
+  memmove((void *)pa2, (void *)pa1, PGSIZE);
+
+  kfree((void *)pa1);
+
+  *pte = PA2PTE(pa2) | PTE_V | PTE_U | PTE_R | PTE_W | PTE_X;
+
+  return 0;
+}
+
 //
 // handle an interrupt, exception, or system call from user space.
 // called from trampoline.S
@@ -67,7 +99,14 @@ usertrap(void)
     syscall();
   } else if((which_dev = devintr()) != 0){
     // ok
-  } else {
+  } else if(r_scause() == 0xf || r_scause() == 12){
+      uint64 va = r_stval(); 
+      if (cowfault(p->pagetable, va) != 0)
+      {
+        p->killed = 1;
+      }
+      
+  }else {
     printf("usertrap(): unexpected scause %p pid=%d\n", r_scause(), p->pid);
     printf("            sepc=%p stval=%p\n", r_sepc(), r_stval());
     p->killed = 1;
